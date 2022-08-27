0beaf804776be6a76277128a13c28ce2c318e742
amodeose <amodeosa@gmail.com>
2022-08-27 19:12:41 -0400

    initial commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import classes from './Calculator.module.css';
+
+const Calculator = props =>  {
+
+    return (
+        <div className={classes.body}>
+            <div className={classes.screen}></div>
+        </div>
+    )
+
+};
+
+export default Calculator;
